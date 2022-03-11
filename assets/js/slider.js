beb7dfb5d4160473b15160af39523d49aa7d1ca6
igver <100843269+igver@users.noreply.github.com>
2022-03-11 18:52:24 +0100

    Add files via upload

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,111 +1,99 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  const buttonPrev = $('.prev');
+  const buttonNext = $('.next');
 
-	const buttonPrev = $('.prev');
-	const buttonNext = $('.next');
+  function getActiveSlide(currentSlider){
+    let sliderElement = $('#' + currentSlider)[0];
+    let activeSlide = $(sliderElement).find('.active')[0];
+    return activeSlide;
+  }
 
-	function getActiveSlide(currentSlider){
-		let sliderElement = document.getElementById(currentSlider);
-		let activeSlide = sliderElement.querySelector('.active');
-		return activeSlide;
-	}
+  function getFirstSlide(currentSlider) {
+    let sliderElement = $('#' + currentSlider)[0];
+    let firstSlide = $(sliderElement).children()[0];
+    return firstSlide;
+  }
 
-	function getFirstSlide(currentSlider){
-		let sliderElement = document.getElementById(currentSlider);
-		let firstSlide = sliderElement.children[0];
-		return firstSlide;
-	}
+  function getSlideBeforeLast(currentSlider) {
+    let sliderElement = $('#' + currentSlider)[0];
+    let activeSlide = $(sliderElement).find('.active')[0];
+    let activeIndex = $(activeSlide).index();
+    activeIndex -= 1;
+    let SlideBeforeLast = $(sliderElement).children().eq(activeIndex);
+    return SlideBeforeLast;
+  }
 
-	function getSlideBeforeLast(currentSlider){
-		let sliderElement = document.getElementById(currentSlider);
-		let activeSlide = sliderElement.querySelector('.active');
-		let activeIndex = $(activeSlide).index();
-		
-		activeIndex -= 1;
-		let SlideBeforeLast = sliderElement.children[activeIndex];
-		return SlideBeforeLast;
-	}
+  function getSlideAfterLast(currentSlider) {
+    let sliderElement = $('#' + currentSlider)[0];
+    let activeSlide = $(sliderElement).find('.active')[0];
+    let activeIndex = $(activeSlide).index();
+    activeIndex += 1;
+    let SlideAfterLast = $(sliderElement).children().eq(activeIndex);
+    return SlideAfterLast;
+  }
 
-	function getSlideAfterLast(currentSlider){
-		let sliderElement = document.getElementById(currentSlider);
-		let activeSlide = sliderElement.querySelector('.active');
-		let activeIndex = $(activeSlide).index();
-		
-		activeIndex += 1;
-		let SlideAfterLast = sliderElement.children[activeIndex];
-		return SlideAfterLast;
-	}
+  function prevSlide(slider) {
+    let activeSlide = getActiveSlide(slider);
+    let activeWidth = $(activeSlide).width();
+    let nextActiveSlide = getSlideBeforeLast(slider);
+    const clone = $(activeSlide).clone();
+    $(clone).removeClass("active");
+    let sliderParent = $('#' + slider)[0];
+    $(sliderParent).prepend(clone);
+    let totalMove = 0 - activeWidth - 10;
+    $("#" + slider).animate({
+      right: totalMove
+    });
+    $(activeSlide).removeClass("active");
+    $(nextActiveSlide).addClass("active");
+    $(buttonPrev).prop('disabled', true);
+    $(buttonNext).prop('disabled', true);
+    setTimeout(function () {
+      sliderParent.removeChild(activeSlide);
+      $("#" + slider).css({
+        right: 0
+      });
+      $(buttonPrev).prop('disabled', false);
+      $(buttonNext).prop('disabled', false);
+    }, 500);
+  }
 
-	function prevSlide(slider){
-		let activeSlide = getActiveSlide(slider);
-		let activeWidth = activeSlide.offsetWidth;
-		let nextActiveSlide = getSlideBeforeLast(slider);
-		const clone = activeSlide.cloneNode(true);
+  function nextSlide(slider) {
+    let firstSlide = getFirstSlide(slider);
+    let activeSlide = getActiveSlide(slider);
+    let firstSlideWidth = $(firstSlide).width();
+    const clone = $(firstSlide).clone();
+    let sliderParent = $('#' + slider)[0];
+    let currentPosition = 0 - firstSlideWidth - 10;
+    $("#" + slider).css({
+      right: currentPosition
+    });
+    $(sliderParent).append(clone);
+    let totalMove = currentPosition + firstSlideWidth + 10;
+    $("#" + slider).animate({
+      right: totalMove
+    });
+    let nextActiveSlide = getSlideAfterLast(slider);
+    $(nextActiveSlide).addClass("active");
+    $(activeSlide).removeClass("active");
+    $(buttonPrev).prop('disabled', true);
+    $(buttonNext).prop('disabled', true);
+    setTimeout(function () {
+      $(sliderParent).find('img:first').remove();
+      $("#" + slider).css({
+        right: 0
+      });
+      $(buttonPrev).prop('disabled', false);
+      $(buttonNext).prop('disabled', false);
+    }, 500);
+  }
 
-		$(clone).removeClass("active");
-
-		let sliderParent = document.getElementById(slider);
-		sliderParent.insertAdjacentElement ('afterBegin', clone);
-
-		let totalMove = 0 - activeWidth - 10;
-		$("#" + slider).animate({right: totalMove});
-
-		$(activeSlide).removeClass("active");
-		$(nextActiveSlide).addClass("active");
-
-		let buttonTop = document.querySelector('.prev');
-		let buttonBottom = document.querySelector('.next');
-		buttonTop.disabled = true;
-		buttonBottom.disabled = true;
-	
-		setTimeout(function () {
-			sliderParent.removeChild(activeSlide);
-			$("#" + slider).css({right: 0});
-			buttonTop.disabled = false;
-			buttonBottom.disabled = false;
-		}, 500);
-	}
-
-	function nextSlide(slider){
-		let firstSlide = getFirstSlide(slider);
-		let activeSlide = getActiveSlide(slider);
-		let firstSlideWidth = firstSlide.offsetWidth;
-
-		const clone = firstSlide.cloneNode(true);
-
-		let sliderParent = document.getElementById(slider);
-		let currentPosition =  0 - firstSlideWidth - 10;
-
-		$("#" + slider).css({right: currentPosition});
-		sliderParent.insertAdjacentElement ('beforeEnd', clone);
-
-		let totalMove =  currentPosition + firstSlideWidth + 10;
-		$("#" + slider).animate({right: totalMove});
-
-		let nextActiveSlide = getSlideAfterLast(slider);
-
-		$(nextActiveSlide).addClass("active");
-		$(activeSlide).removeClass("active");
-
-		let buttonTop = document.querySelector('.prev');
-		let buttonBottom = document.querySelector('.next');
-		buttonTop.disabled = true;
-		buttonBottom.disabled = true;
-
-		setTimeout(function () {
-			sliderParent.removeChild(firstSlide);
-			$("#" + slider).css({right: 0});
-			buttonTop.disabled = false;
-			buttonBottom.disabled = false;
-		}, 500);
-	}
-
-	buttonPrev.on('click', () => {
-		prevSlide('slider-top');
-		prevSlide('slider-bottom');
-	});
-
-	buttonNext.on('click', () => {
-		nextSlide('slider-top');
-		nextSlide('slider-bottom');
-	});
+  buttonPrev.on('click', () => {
+    prevSlide('slider-top');
+    prevSlide('slider-bottom');
+  });
+  buttonNext.on('click', () => {
+    nextSlide('slider-top');
+    nextSlide('slider-bottom');
+  });
 });
